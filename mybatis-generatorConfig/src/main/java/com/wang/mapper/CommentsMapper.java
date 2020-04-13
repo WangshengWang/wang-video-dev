1979ef43df6b3c9e6de4e56cd5c8bd23402bb40b
wangsheng@qq.com <wangsheng@qq.com>
2020-04-13 22:23:16 +0800

    修改

---
+++ mybatis-generatorConfig/src/main/java/com/wang/mapper/CommentsMapper.java
@@ -0,0 +1,7 @@
+package com.wang.mapper;
+
+import com.wang.pojo.Comments;
+import com.wang.utils.MyMapper;
+
+public interface CommentsMapper extends MyMapper<Comments> {
+}
---
+++ mybatis-generatorConfig/src/main/java/com/wang/mapper/SearchRecordsMapper.java
@@ -0,0 +1,7 @@
+package com.wang.mapper;
+
+import com.wang.pojo.SearchRecords;
+import com.wang.utils.MyMapper;
+
+public interface SearchRecordsMapper extends MyMapper<SearchRecords> {
+}
